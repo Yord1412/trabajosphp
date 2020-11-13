b761990f165ba4b2fd3cb59845403fce21118ac5
Yord1412 <71461787+Yord1412@users.noreply.github.com>
2020-11-12 22:06:41 -0700

    ejercicio 1 php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	$name = "jordy";
+	$lastName = "Segoviano";
+	$FullName = $name." ".$lastName;
+?>
+
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<TITLE>
+			SITE
+		</TITLE>
+	</HEAD>
+	<BODY>
+		<h1>
+			hola mi nombre es <?= $FullName; ?>
+		</h1>
+
+	</BODY>
+</HTML>
